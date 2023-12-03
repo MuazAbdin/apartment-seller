fb4b7b69fd7f3d62fdbb796a08c1432bef1968a5
Muaz Abdin <muathabd25@gmail.com>
2023-12-03 15:14:13 +0200

    complete HandelBars Ex

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
-
 $("button").on("click", function () {
-    let address = $("#addr-input").val()
-    let minPrice = $("#min-p-input").val()
-    let maxPrice = $("#max-p-input").val()
-    let minRooms = $("#min-r-input").val()
-    let maxRooms = $("#max-r-input").val()
-    let immediate = $("#immed-y")
+  let address = $("#addr-input").val();
+  let minPrice = $("#min-p-input").val();
+  let maxPrice = $("#max-p-input").val();
+  let minRooms = $("#min-r-input").val();
+  let maxRooms = $("#max-r-input").val();
+  let immediate = $("#immed-y");
+  let parking = $("#park-y");
 
-    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate)
-    renderApts(relevantApts)
-})
+  let relevantApts = findRelevantApts(
+    address,
+    minPrice,
+    maxPrice,
+    minRooms,
+    maxRooms,
+    immediate,
+    parking
+  );
+  renderApts(relevantApts);
+});
+
+const source = $("#apartment-template").html();
+const template = Handlebars.compile(source);
 
 const renderApts = function (apartments) {
-    $("#results").empty()
-    console.log(apartments) //array of apartments to render
-    //Your code goes here.
-}
+  $("#results").empty();
+  console.log(apartments); //array of apartments to render
+  $("#results").append(template({ apartments }));
+};
 
-renderApts(apartments) //renders apartments when page loads
+renderApts(apartments); //renders apartments when page loads
